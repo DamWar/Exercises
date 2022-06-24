30f1ddb0860e71b5a6cba65c148b3a735ba1b7c5
unknown <damiwar043@student.polsl.pl>
2022-06-24 22:34:55 +0200

    completed input handling

--- C++/BankPanels/bankPanels.cpp
+++ C++/BankPanels/bankPanels.cpp
@@ -19,7 +19,8 @@ public:
 	}
 };
 
-//current operations[0]: 0 = deposit, 1 = withdraw, 2 = outgoing transfer, 3 = incoming transfer, 4 = takeLoan, 5 = SplacKredyt
+//current operations[0]: 0 = deposit, 1 = withdraw, 2 = outgoing transfer, 3 = incoming transfer
+//4 = takeLoan, 5 = SplacKredyt
 
 class User : public Account { // ADD locked and logged handling
 public:
@@ -126,6 +127,7 @@ public:
 
 class Admin : public Account {
 public:
+	vector<User> usersWithDebt; //task requirement
 	Admin(string passwd) : Account(passwd) {
 		//pass
 	}
@@ -140,6 +142,33 @@ public:
 	}
 	double countAllBankUsersMoney(Bank b) {
 		return b.countAllUsersMoney();
+	}
+	double countAllUsersDebtValue() {
+		double sum = 0;
+		for (auto user : usersWithDebt) {
+			sum += user.kredyt;
+		}
+		return sum;
+	}
+	bool addUserToVectorWithDebt(User u) {
+		for (auto user : usersWithDebt) {
+			if (user.id == u.id) {
+				return false;
+			}
+		}
+		usersWithDebt.push_back(u);
+		return true;
+	}
+	void deleteUserFromVectorWithDebt(User u) {
+		long long int i = 0;
+		for (auto user : usersWithDebt) {
+			if (user.id == u.id) {
+				if(u.kredyt <= 0)
+					usersWithDebt.erase(usersWithDebt.begin() + i);
+				return;
+			}
+			++i;
+		}
 	}
 };
 
@@ -185,7 +214,7 @@ int main() {
 	int inputInt;
 	if (userOrAdmin == 1) {
 		while (cin >> inputInt && bankObj.findIdIndex(inputInt) == -1) {
-			cout << inputInt << "Identifier not found, please provide correct identifier.\n";
+			cout << "Identifier not found, please provide correct identifier.\n";
 		}
 		cout << "Please provide your password.\n";
 		long long int currentBankId = bankObj.findIdIndex(inputInt);
@@ -207,17 +236,15 @@ int main() {
 		cout << "Logged in.\n";
 	}
 
-	
-
 	if (userOrAdmin == 1) {
+		long long int tempBankId;
+		double tempAmount;
 		cout << "Your current balance is: " << currentUser->currentMoney
 			<< "\nWhat would you like to do?\n Deposit - 1\n withdraw - 2\n transfer - 3\n "
-			<< "check operation history - 4\n generate sum of all despoitsand withdraws - 5\n "
+			<< "check operation history - 4\n generate sum of all despoits and withdraws - 5\n "
 			<< "take a loan - 6\n pay off the loan - 7\n log off and quit - 8\n";
 		while (cin >> inputInt) {
 			switch (inputInt) {
-				long long int tempBankId;
-				double tempAmount;
 				case 1:
 					cout << "Please specify how much you'd like to deposit.\n";
 					cin >> tempAmount;
@@ -243,37 +270,138 @@ int main() {
 					currentUser->transfer(tempAmount, bankObj.accounts[tempBankId]);
 					cout << "Your new balance is: " << currentUser->currentMoney << "\n";
 					break;
+				//current operations[0]: 0 = deposit, 1 = withdraw, 2 = outgoing transfer, 3 = incoming transfer
+				//4 = takeLoan, 5 = SplacKredyt
 				case 4:
-
+					cout << "Type of operation \t| Amount \t| Target\n";
+					for (auto operation : currentUser->checkHistory()) {
+						switch (operation.first.first)
+						{
+							case 0:
+								cout << "Deposit \t\t| " << operation.first.second << " \t| "
+									<< operation.second << "\n";
+								break;
+							case 1:
+								cout << "Withdraw \t\t| " << operation.first.second << " \t| "
+									<< operation.second << "\n";
+								break;
+							case 2:
+								cout << "Outgoing transfer \t| " << operation.first.second << " \t| "
+									<< operation.second << "\n";
+								break;
+							case 3:
+								cout << "Incoming transfer \t| " << operation.first.second << " \t| "
+									<< operation.second << "\n";
+								break;
+							case 4:
+								cout << "Taken loan \t| " << operation.first.second << " \t| "
+									<< operation.second << "\n";
+								break;
+							case 5:
+								cout << "Paid off loan \t| " << operation.first.second << " \t| "
+									<< operation.second << "\n";
+								break;
+							default:
+								cout << "Corrupted operation.\n";
+								break;
+						}
+					}
 					break;
 				case 5:
-
+					cout << "Total deposits: " << currentUser->generateSummary()[0] << "\nTotal wihdraws: "
+						<< currentUser->generateSummary()[1] << "\n";
 					break;
 				case 6:
-
+					cout << "Please specify how high would you like to loan to be.\n";
+					cin >> tempAmount;
+					currentUser->takeLoan(tempAmount); //... there was no verification specified so I'll leave it as it is
+					adm.addUserToVectorWithDebt(*currentUser);
+					cout << "Your new balance is: " << currentUser->currentMoney << "\n";
 					break;
 				case 7:
-
+					cout << "Please specify how much you'd like to pay back\n";
+					cin >> tempAmount;
+					currentUser->SplacKredyt(tempAmount); //... there was no verification specified so I'll leave it as it is
+					adm.deleteUserFromVectorWithDebt(*currentUser);
+					cout << "Your new balance is: " << currentUser->currentMoney << "\n";
 					break;
 				case 8:
-					cout << "Good bye. We hope you have a nice day";
+					cout << "Good bye. We hope you have a nice day\n";
 					return 0;
 				default:
-					cout << "Unknown number. Please try again.";
+					cout << "Unknown number. Please try again.\n";
 					break;
 			}
-			cout << "What would you like to do next ? \n Deposit - 1\n withdraw - 2\n transfer - 3\n "
+			cout << "What would you like to do next? \n Deposit - 1\n withdraw - 2\n transfer - 3\n "
 				<< "check operation history - 4\n generate sum of all despoitsand withdraws - 5\n "
-				<< "take a loan - 6\n pay off the loan - 7\n log off and quit - 8\n";
+				<< "take a loan - 6\n pay off the loan - 7\n log out and quit - 8\n";
 		}
 	}
 	if (userOrAdmin == 2) {
+		long long int tempBankId;
+		string tempPasswd;
 		cout << "What would you like to do?\n Lock an account - 1\n add an account - 2\n delete an account - 3\n "
 			<< "check how much money there is in a bank - 4\n check all debts issued - 5\n "
-			<< "show all debtors - 6\n";
+			<< "show all debtors - 6\n log out and quit - 7\n";
+		while (cin >> inputInt) {
+			switch (inputInt)
+			{
+			case 1:
+				cout << "Please specify the id of the account you'd like to disable.\n";
+				cin >> inputInt;
+				tempBankId = bankObj.findIdIndex(inputInt);
+				if (tempBankId == -1) {
+					cout << "Invalid identifier.\n";
+					break;
+				}
+				adm.lockUserAccount(bankObj.accounts[tempBankId]);
+				cout << "Account successfully disabled.\n";
+				break;
+			case 2:{
+				cout << "That account's identifier is going to be " << idCount + 1
+					<< "\nPlease provide a new password for that user.\n";
+				cin >> tempPasswd;
+				User newU(tempPasswd);
+				adm.addAccount(newU, bankObj);
+				cout << "Account succesfully added\n";
+				break;
+			}
+			case 3:
+				cout << "Please specify the id of the account you'd like to delete.\n";
+				cin >> inputInt;
+				tempBankId = bankObj.findIdIndex(inputInt);
+				if (tempBankId == -1) {
+					cout << "Invalid identifier.\n";
+					break;
+				}
+				adm.deleteAccount(bankObj, inputInt);
+				cout << "Account successfully deleted from the bank.\n";
+				break;
+			case 4:
+				cout << "There is " << adm.countAllBankUsersMoney(bankObj) << " money inside the bank\n";
+				break;
+			case 5:
+				cout << "There is " << adm.countAllUsersDebtValue() << " money worth of debts\n";
+				break;
+			case 6:
+				cout << "Identifiers of accounts with debts are:\n";
+				for (auto user : adm.usersWithDebt) {
+					cout << user.id << ", ";
+				}
+				cout << "\n";
+				break;
+			case 7:
+				cout << "See you next time.\n";
+				return 0;
+			default:
+				cout << "Unknown number. Please try again.\n";
+				break;
+			}
+			cout << "What would you like to do next?\n Lock an account - 1\n add an account - 2\n delete an account - 3\n "
+				<< "check how much money there is in a bank - 4\n check all debts issued - 5\n "
+				<< "show all debtors - 6\n log out and quit - 7\n";
+		}
 	}
-
-	
 
 	return 0;
 }
