faaadbbba9212dc6856567f75ecd4fb691190476
DamWar <d.warzecha@poczta.fm>
2022-08-29 05:25:03 +0200

    added scripts for App Service creation

---
+++ DevOps-sessions/infrastructure-01/azure-cli/session-08/script2.sh
@@ -0,0 +1,38 @@
+parameterFile="script2.json";
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -h | --help )       echo "Provide path to parameters file, eg. ./script2.sh script2.json"
+                            ;;
+        * )                 parameterFile=$1
+                            ;;
+    esac
+    shift
+done
+
+region="$(cat $parameterFile | jq -r '.region')";
+resourceGroup="$(cat $parameterFile | jq -r '.resourceGroup')"
+appName="$(cat $parameterFile | jq -r '.appName')";
+appTier="$(cat $parameterFile | jq -r '.appTier')";
+instanceCount="$(cat $parameterFile | jq -r '.instanceCount')";
+dockerImage="$(cat $parameterFile | jq -r '.dockerImage')";
+planName="$appName-plan";
+
+az appservice plan create                           \
+    --name $planName                                \
+    --resource-group $resourceGroup                 \
+    --sku $appTier                                  ;
+
+az webapp create                                    \
+    --name $appName                                 \
+    --plan $planName                                \
+    --resource-group $resourceGroup                 \
+    --deployment-container-image-name $dockerImage  ;
+
+if [ $instanceCount -gt 1 ]; then
+    az webapp scale                                 \
+        --instance-count $instanceCount             \
+        --name $appName                             \
+        --resource-group $resourceGroup             ;
+fi
+
