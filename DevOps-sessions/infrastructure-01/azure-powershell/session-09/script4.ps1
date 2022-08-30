fb9d260db3a01175b4d4e70268e7c4a3fe0ca9ff
DamWar <d.warzecha@poczta.fm>
2022-08-30 19:56:04 +0200

    refactor and improvements

--- DevOps-sessions/infrastructure-01/azure-powershell/session-09/script4.ps1
+++ DevOps-sessions/infrastructure-01/azure-powershell/session-09/script4.ps1
@@ -1,7 +1,14 @@
 param([string]$parameterFile='script4.json')
 $parameters = cat $parameterFile | ConvertFrom-Json
 
-$resources = Get-AzResource -ResourceGroupName $parameters.resourceGroup
-foreach ($resource in $resources) {
-    Remove-AzResource -ResourceId $resource.id -Force
+$safetyCount = 0
+
+while(($resources = Get-AzResource -ResourceGroupName $parameters.resourceGroup) -and ($resources -ne "")){
+    foreach ($resource in $resources) {
+        Remove-AzResource -ResourceId $resource.id -Force
+    }
+    if($safetyCount++ -ge 5){
+        echo "Script executed unexpected amount of times. Please make sure you want to continue and execute the script again if needed."
+        exit 0
+    }
 }
