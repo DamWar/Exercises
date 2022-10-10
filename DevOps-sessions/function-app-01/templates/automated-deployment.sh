a4383a47065b242069dddc8947688397714ecad0
DamWar <d.warzecha@poczta.fm>
2022-10-10 02:15:18 +0200

    Modified whole project to deploy to function app directly
    Before this commit, it uploaded the function code to the storage container. Function app then attempted to read it from the storage. Now it uses az functionapp deployment to deploy zipped package to the function app directly

--- DevOps-sessions/function-app-01/templates/automated-deployment.sh
+++ DevOps-sessions/function-app-01/templates/automated-deployment.sh
@@ -1,19 +1,8 @@
 resourceGroup="infrastructure-01"
-
-
 templateFile="function"
 
 az deployment group create                          \
     --name "add-function-app"                       \
-    --resource-group "$resourceGroup"               \
-    --template-file "$templateFile.json"            \
-    --parameters "$templateFile-parameters.json"    \
-    --verbose
-
-templateFile="role"
-
-az deployment group create                          \
-    --name "add-function-app-role"                  \
     --resource-group "$resourceGroup"               \
     --template-file "$templateFile.json"            \
     --parameters "$templateFile-parameters.json"    \
