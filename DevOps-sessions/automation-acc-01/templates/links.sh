629616dc7d603b68dfba57e915b2b331083374af
DamWar <d.warzecha@poczta.fm>
2022-10-12 20:25:36 +0200

    Added links between runbook and schedules

---
+++ DevOps-sessions/automation-acc-01/templates/links.sh
@@ -0,0 +1,15 @@
+deploy () {
+    az deployment group create                      \
+    --name "$1"                                     \
+    --resource-group "$2"                           \
+    --template-file "$3.json"                       \
+    --parameters "parameters.json"                  \
+    --verbose
+}
+resourceGroup="infrastructure-01"
+deployAutomation () {
+    deploy "add-automation-$1" $resourceGroup "$1" 
+}
+
+
+deployAutomation "links"
